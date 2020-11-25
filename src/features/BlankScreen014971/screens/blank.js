b614b39e5db8ef64a80669d7707a6096a56163fe
Crowdbotics <team@crowdbotics.com>
2020-11-25 15:00:58 +0000

    Update components

--- src/features/BlankScreen014971/screens/blank.js
+++ src/features/BlankScreen014971/screens/blank.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"
 import {
   View,
   Image,
@@ -8,33 +8,79 @@ import {
   Button,
   Switch,
   TextInput,
-  StyleSheet,
-} from "react-native";
-import DateTimePicker from 'react-native-datepicker';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import Slider from '@react-native-community/slider';
-import { CheckBox } from 'react-native-elements';
-import {SlideMenuIcon} from '../../../navigator/slideMenuIcon';
+  StyleSheet
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
 
 export default class Blank extends React.Component {
-  static navigationOptions = ({navigation}) => {
+  static navigationOptions = ({ navigation }) => {
     return {
-      headerLeft: <SlideMenuIcon navigationProps={navigation} />,
-    };
-  };
-  
-  state = {};
+      headerLeft: <SlideMenuIcon navigationProps={navigation} />
+    }
+  }
+
+  state = { DateTimePicker_6: new Date(""), TextInput_7: "", Switch_9: true }
 
   render = () => (
-    <View style={styles.container}>
-      <Text>This is your new component</Text>
+    <View>
+      <Icon name="star" />
+      <Icon name="star" />
+      <Icon name="star" />
+      <Text>Sample text content</Text>
+      <DateTimePicker
+        showIcon={false}
+        date={this.state.DateTimePicker_6}
+        onDateChange={selectedDate =>
+          this.setState({ DateTimePicker_6: selectedDate })
+        }
+      />
+      <TextInput
+        placeholder="Sample text input placeholder"
+        multiline={true}
+        value={this.state.TextInput_7}
+        onChangeText={nextValue => this.setState({ TextInput_7: nextValue })}
+      />
+      <Slider value={50} minimumValue={0} maximumValue={100} />
+      <Switch
+        trackColor={{ false: "#C0CCDA", true: "#409EFF" }}
+        style={styles.Switch_9}
+        value={this.state.Switch_9}
+        onValueChange={nextChecked => this.setState({ Switch_9: nextChecked })}
+      />
+      <Image source={{ uri: "https://via.placeholder.com/150" }} />
+      <Icon name="star" />
+      <Icon name="star" />
+      <Icon name="star" />
+      <Icon name="star" />
+      <Icon name="star" />
+      <Icon name="star" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginHorizontal: 16,
+    marginHorizontal: 16
   },
-});
+  View_1: {},
+  Icon_2: {},
+  Icon_3: {},
+  Icon_4: {},
+  Text_5: {},
+  DateTimePicker_6: {},
+  TextInput_7: {},
+  Slider_8: {},
+  Switch_9: { alignSelf: "flex-start" },
+  Image_10: {},
+  Icon_11: {},
+  Icon_12: {},
+  Icon_13: {},
+  Icon_14: {},
+  Icon_15: {},
+  Icon_16: {}
+})
