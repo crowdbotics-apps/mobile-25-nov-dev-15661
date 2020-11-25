613f5008bd061db9712fcbc98e06ab16fa43d4af
Crowdbotics <team@crowdbotics.com>
2020-11-25 08:55:22 +0000

    Update components

--- src/features/BlankScreen014947/screens/blank.js
+++ src/features/BlankScreen014947/screens/blank.js
@@ -23,13 +23,35 @@ export default class Blank extends React.Component {
     }
   }
 
-  state = {}
+  state = { DateTimePicker_6: new Date(""), TextInput_7: "", Switch_9: true }
 
   render = () => (
     <View>
       <Icon name="star" />
       <Icon name="star" />
       <Icon name="star" />
+      <Text>Sample text content</Text>
+      <DateTimePicker
+        showIcon={false}
+        date={this.state.DateTimePicker_6}
+        onDateChange={selectedDate =>
+          this.setState({ DateTimePicker_6: selectedDate })
+        }
+      />
+      <TextInput
+        placeholder="Sample text input placeholder"
+        multiline={true}
+        value={this.state.TextInput_7}
+        onChangeText={nextValue => this.setState({ TextInput_7: nextValue })}
+      />
+      <Slider value={50} minimumValue={0} maximumValue={100} />
+      <Switch
+        trackColor={{ false: "#C0CCDA", true: "#409EFF" }}
+        style={styles.Switch_9}
+        value={this.state.Switch_9}
+        onValueChange={nextChecked => this.setState({ Switch_9: nextChecked })}
+      />
+      <Image source={{ uri: "https://via.placeholder.com/150" }} />
     </View>
   )
 }
@@ -68,5 +90,15 @@ const styles = StyleSheet.create({
   View_1: {},
   Icon_2: {},
   Icon_3: {},
-  Icon_4: {}
+  Icon_4: {},
+  View_1: {},
+  Icon_2: {},
+  Icon_3: {},
+  Icon_4: {},
+  Text_5: {},
+  DateTimePicker_6: {},
+  TextInput_7: {},
+  Slider_8: {},
+  Switch_9: { alignSelf: "flex-start" },
+  Image_10: {}
 })
